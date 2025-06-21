4019d90756325d124a170f3f68b9e837accb1ada
Muhammad Ma'mun Efendi <muhammadefendi002@gmail.com>
2025-06-21 14:57:02 +0700

    fix typeorm

--- apps/api/src/config/typeorm.config.ts
+++ apps/api/src/config/typeorm.config.ts
@@ -1,4 +1,3 @@
-// src/config/typeorm.config.ts
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from '../users/entities/user.entity';
@@ -7,15 +6,29 @@ import { MoodEntry } from '../mood-entry/entities/mood-entry.entity';
 export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
   inject: [ConfigService],
-  useFactory: (configService: ConfigService) => ({
-    type: 'mysql',
-    host: configService.get<string>('DATABASE_HOST'),
-    port: configService.get<number>('DATABASE_PORT'),
-    username: configService.get<string>('DATABASE_USERNAME'),
-    password: configService.get<string>('DATABASE_PASSWORD'),
-    database: configService.get<string>('DATABASE_NAME'),
-    entities: [User, MoodEntry], // Tambahkan entity lain jika ada
-    synchronize: configService.get<string>('NODE_ENV') !== 'production', // Aman untuk dev
-    autoLoadEntities: true,
-  }),
+  useFactory: (configService: ConfigService) => {
+    const databaseUrl = configService.get<string>('DATABASE_URL');
+
+    if (databaseUrl) {
+      return {
+        type: 'mysql',
+        url: databaseUrl,
+        entities: [User, MoodEntry],
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        autoLoadEntities: true,
+      };
+    }
+
+    return {
+      type: 'mysql',
+      host: configService.get<string>('DATABASE_HOST'),
+      port: configService.get<number>('DATABASE_PORT'),
+      username: configService.get<string>('DATABASE_USERNAME'),
+      password: configService.get<string>('DATABASE_PASSWORD'),
+      database: configService.get<string>('DATABASE_NAME'),
+      entities: [User, MoodEntry],
+      synchronize: configService.get<string>('NODE_ENV') !== 'production',
+      autoLoadEntities: true,
+    };
+  },
 };
